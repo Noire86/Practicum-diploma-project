93067c27982ebf846fe3607b17dc4eef82a4dd72
Noire86 <gleb.iwanow2010@yandex.ru>
2022-10-20 05:21:15 +0500

    [Feature] utility features

--- ewm-main/src/main/java/ru/soular/ewm/AppConfiguration.java
+++ ewm-main/src/main/java/ru/soular/ewm/AppConfiguration.java
@@ -3,12 +3,25 @@ package ru.soular.ewm;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.soular.ewm.category.dto.CategoryDto;
+import ru.soular.ewm.category.model.Category;
+import ru.soular.ewm.event.dto.EventFullDto;
+import ru.soular.ewm.event.model.Event;
+import ru.soular.ewm.user.dto.UserDto;
+import ru.soular.ewm.user.dto.UserShortDto;
+import ru.soular.ewm.user.model.User;
 
 @Configuration
 public class AppConfiguration {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
+        mapper.createTypeMap(User.class, UserDto.class);
+        mapper.createTypeMap(User.class, UserShortDto.class);
+        mapper.createTypeMap(Category.class, CategoryDto.class);
+        mapper.createTypeMap(Event.class, EventFullDto.class);
+
+        return mapper;
     }
 }

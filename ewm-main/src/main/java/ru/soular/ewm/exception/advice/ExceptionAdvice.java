627f625cb56a18f6075b16a967944a7b9266a920
Noire86 <gleb.iwanow2010@yandex.ru>
2022-10-20 05:24:51 +0500

    [Feature] reworked exception functionality

--- ewm-main/src/main/java/ru/soular/ewm/exception/advice/ExceptionAdvice.java
+++ ewm-main/src/main/java/ru/soular/ewm/exception/advice/ExceptionAdvice.java
@@ -2,24 +2,53 @@ package ru.soular.ewm.exception.advice;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ru.soular.ewm.exception.model.ApplicationException;
 import ru.soular.ewm.exception.model.ExceptionResponse;
-import ru.soular.ewm.exception.model.ValidationException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import org.hibernate.exception.ConstraintViolationException;
+import ru.soular.ewm.util.Constants;
 
 import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionAdvice extends ResponseEntityExceptionHandler {
 
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+    @Override
+    @NonNull
+    public ResponseEntity<Object> handleExceptionInternal(
+            @NonNull Exception ex, @Nullable Object body, @NonNull HttpHeaders headers, @NonNull HttpStatus status,
+            @NonNull WebRequest request) {
+        List<String> errors = new ArrayList<>();
+
+        if (ex instanceof BindException) {
+            ((BindException) ex).getFieldErrors().forEach(err -> errors.add(err.getField() + ": " + err.getDefaultMessage()));
+            ((BindException) ex).getGlobalErrors().forEach(err -> errors.add(err.getObjectName() + ": " + err.getDefaultMessage()));
+        }
+
+        ExceptionResponse response = ExceptionResponse.builder()
+                .errors(errors)
+                .status(HttpStatus.BAD_REQUEST.name())
+                .reason("For the requested operation the conditions are not met.")
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now().format(Constants.FORMATTER))
+                .build();
+
+        log.warn(String.format("%s is thrown: %s", ex.getClass().getSimpleName(), ex.getMessage()));
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
@@ -27,7 +56,8 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
                 .status(HttpStatus.CONFLICT.name())
                 .reason("Integrity constraint has been violated")
                 .message(ex.getSQLException().getMessage())
-                .timestamp(LocalDateTime.now().format(formatter))
+                .timestamp(LocalDateTime.now().format(Constants.FORMATTER))
+                .errors(new ArrayList<>())
                 .build();
 
         log.warn(String.format("%s is thrown: %s", ex.getClass().getSimpleName(), ex.getMessage()));
@@ -40,10 +70,23 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
                 .status(HttpStatus.NOT_FOUND.name())
                 .reason("The required object was not found.")
                 .message(ex.getMessage())
-                .timestamp(LocalDateTime.now().format(formatter))
+                .timestamp(LocalDateTime.now().format(Constants.FORMATTER))
                 .build();
 
         log.warn(String.format("%s is thrown: %s", ex.getClass().getSimpleName(), ex.getMessage()));
-        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<Object> handleEventException(ApplicationException ex) {
+        ExceptionResponse response = ExceptionResponse.builder()
+                .status(ex.getCode().name())
+                .reason("For the requested operation the conditions are not met.")
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now().format(Constants.FORMATTER))
+                .build();
+
+        log.warn(String.format("%s is thrown: %s", ex.getClass().getSimpleName(), ex.getMessage()));
+        return new ResponseEntity<>(response, ex.getCode());
     }
 }

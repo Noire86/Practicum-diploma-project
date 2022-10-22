91c54c658c5285c9e1783455800211266d46ae46
Soularx86 <gleb.iwanow2010@yandex.ru>
2022-10-23 01:42:26 +0500

    [Feature] mapping settings for participationRequest

--- ewm-main/src/main/java/ru/soular/ewm/main/AppConfiguration.java
+++ ewm-main/src/main/java/ru/soular/ewm/main/AppConfiguration.java
@@ -7,6 +7,8 @@ import ru.soular.ewm.main.category.dto.CategoryDto;
 import ru.soular.ewm.main.category.model.Category;
 import ru.soular.ewm.main.event.dto.EventFullDto;
 import ru.soular.ewm.main.event.model.Event;
+import ru.soular.ewm.main.participation.dto.ParticipationRequestDto;
+import ru.soular.ewm.main.participation.model.ParticipationRequest;
 import ru.soular.ewm.main.user.dto.UserDto;
 import ru.soular.ewm.main.user.dto.UserShortDto;
 import ru.soular.ewm.main.user.model.User;
@@ -21,6 +23,9 @@ public class AppConfiguration {
         mapper.createTypeMap(User.class, UserShortDto.class);
         mapper.createTypeMap(Category.class, CategoryDto.class);
         mapper.createTypeMap(Event.class, EventFullDto.class);
+        mapper.createTypeMap(ParticipationRequest.class, ParticipationRequestDto.class)
+                .addMapping(src -> src.getRequester().getId(), ParticipationRequestDto::setRequester)
+                .addMapping(src -> src.getEvent().getId(), ParticipationRequestDto::setEvent);
 
         return mapper;
     }

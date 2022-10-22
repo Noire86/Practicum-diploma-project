be014d19d1e1228abfbd16bd3f47cf186b62039d
Soularx86 <gleb.iwanow2010@yandex.ru>
2022-10-23 01:43:02 +0500

    [Feature] stats exceptions logic

--- ewm-main/src/main/java/ru/soular/ewm/main/client/service/BaseClient.java
+++ ewm-main/src/main/java/ru/soular/ewm/main/client/service/BaseClient.java
@@ -1,9 +1,9 @@
 package ru.soular.ewm.main.client.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.lang.Nullable;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -12,20 +12,28 @@ import java.util.List;
 public class BaseClient {
     protected final RestTemplate rest;
 
-    public <T> ResponseEntity<T> get(String path, ParameterizedTypeReference<T> typeReference) {
-        return sendRequest(HttpMethod.GET, path, null, typeReference);
+    public <T> ResponseEntity<Object> get(String path) {
+        return sendRequest(HttpMethod.GET, path, null);
     }
 
-    public <T> void post(String path, T body, ParameterizedTypeReference<T> typeReference) {
-        sendRequest(HttpMethod.POST, path, body, typeReference);
+    public <T> void post(String path, T body) {
+        sendRequest(HttpMethod.POST, path, body);
     }
 
-    private <T> ResponseEntity<T> sendRequest(HttpMethod method, String path, @Nullable T body, ParameterizedTypeReference<T> typeReference) {
+    private <T> ResponseEntity<Object> sendRequest(HttpMethod method, String path, @Nullable T body) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
 
         HttpEntity<T> entity = new HttpEntity<>(body, headers);
-        return rest.exchange(path, method, entity, typeReference);
+
+        ResponseEntity<Object> response;
+        try {
+            response = rest.exchange(path, method, entity, Object.class);
+
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsString());
+        }
+        return response;
     }
 }
--- ewm-stats/src/main/java/ru/soular/ewm/stats/service/StatsServiceImpl.java
+++ ewm-stats/src/main/java/ru/soular/ewm/stats/service/StatsServiceImpl.java
@@ -3,18 +3,20 @@ package ru.soular.ewm.stats.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import ru.soular.ewm.stats.dao.EndpointHitDAO;
 import ru.soular.ewm.stats.dto.EndpointHitDto;
 import ru.soular.ewm.stats.dto.ViewStatsDto;
+import ru.soular.ewm.stats.exception.model.ApplicationException;
 import ru.soular.ewm.stats.model.EndpointHit;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import static ru.soular.ewm.stats.util.Constants.FORMATTER;
 
@@ -37,23 +39,28 @@ public class StatsServiceImpl implements StatsService {
     @Override
     public List<ViewStatsDto> get(String start, String end, List<String> uris, Boolean unique) {
         List<ViewStatsDto> result = new ArrayList<>();
-        LocalDateTime startTime = null;
-        LocalDateTime endTime = null;
+        LocalDateTime startTime;
+        LocalDateTime endTime;
 
-        if (Objects.nonNull(start)) {
+        try {
             startTime = LocalDateTime.parse(URLDecoder.decode(start, StandardCharsets.UTF_8), FORMATTER);
+        } catch (DateTimeParseException ex) {
+            throw new ApplicationException("Invalid starting date value!", HttpStatus.BAD_REQUEST);
         }
 
-        if (Objects.nonNull(end)) {
+        try {
             endTime = LocalDateTime.parse(URLDecoder.decode(end, StandardCharsets.UTF_8), FORMATTER);
+        } catch (DateTimeParseException ex) {
+            throw new ApplicationException("Invalid ending date value!", HttpStatus.BAD_REQUEST);
+        }
+
+        if (endTime.isBefore(startTime)) {
+            throw new ApplicationException("Ending time is before starting time of the range", HttpStatus.BAD_REQUEST);
         }
 
         log.info("Getting stats for {} range from {} to {} (unique={})", uris, startTime, endTime, unique);
         for (String uri : uris) {
-            List<EndpointHit> hits =
-                    (Objects.nonNull(startTime) && Objects.nonNull(endTime))
-                            ? hitDAO.getEndpointHitsBy(uri, startTime, endTime)
-                            : hitDAO.getAllByUri(uri);
+            List<EndpointHit> hits = hitDAO.getEndpointHitsBy(uri, startTime, endTime);
 
             if (!hits.isEmpty()) {
                 ViewStatsDto stats = ViewStatsDto.builder()

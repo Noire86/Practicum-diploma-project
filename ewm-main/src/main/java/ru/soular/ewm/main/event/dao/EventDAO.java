d7a8aac3840dade25abf71dd8682c4c7fbb31b09
Noire86 <gleb.iwanow2010@yandex.ru>
2022-10-25 21:19:37 +0500

    [Fix] between operator in EventDAO

--- ewm-main/src/main/java/ru/soular/ewm/main/event/dao/EventDAO.java
+++ ewm-main/src/main/java/ru/soular/ewm/main/event/dao/EventDAO.java
@@ -12,8 +12,8 @@ import java.util.List;
 
 public interface EventDAO extends CustomJpaRepository<Event, Long> {
 
-    @Query("select e from Event as e where e.initiator.id in ?1 and e.state in ?2 and e.category.id in ?3 and e.eventDate > ?4 " +
-            "and e.eventDate < ?5")
+    @Query("select e from Event as e where e.initiator.id in ?1 and e.state in ?2 and e.category.id in ?3 " +
+            "and e.eventDate between ?4 and ?5")
     List<Event> getEvents(List<Long> userIds, List<EventState> states, List<Long> categoryIds, LocalDateTime rangeStart,
                           LocalDateTime rangeEnd, Pageable pageable);
 
@@ -22,8 +22,7 @@ public interface EventDAO extends CustomJpaRepository<Event, Long> {
             "or upper(e.description) like upper(concat('%', ?1, '%')) " +
             "and e.category.id in ?2 " +
             "and e.paid = ?3 " +
-            "and e.eventDate >= ?4 " +
-            "and e.eventDate <= ?4 ")
+            "and e.eventDate between ?4 and ?5")
     List<Event> getEvents(String text, List<Long> categoryIds, Boolean paid, LocalDateTime rangeStart,
                           LocalDateTime rangeEnd, Pageable pageable);
 
@@ -32,8 +31,7 @@ public interface EventDAO extends CustomJpaRepository<Event, Long> {
             "or upper(e.description) like upper(concat('%', ?1, '%')) " +
             "and e.category.id in ?2 " +
             "and e.paid = ?3 " +
-            "and e.eventDate >= ?4 " +
-            "and e.eventDate <= ?4 " +
+            "and e.eventDate between ?4 and ?5" +
             "and e.confirmedRequests < e.participantLimit ")
     List<Event> getOnlyAvailableEvents(String text, List<Long> categoryIds, Boolean paid, LocalDateTime rangeStart,
                                        LocalDateTime rangeEnd, Pageable pageable);
